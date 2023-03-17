7d4c422a3f81c9e6caae13e13d26603f8761fbb9
ZabiyakaDaniil <101666279+ZabiyakaDaniil@users.noreply.github.com>
2023-03-17 11:39:40 +0300

    Update SQL запросы.sql

--- de101/module02/SQL запросы.sql
+++ de101/module02/SQL запросы.sql
@@ -10,11 +10,44 @@ from orders
 
 --Profit Ratio
 select
-sum(sales), sum(profit), sum(profit)/sum(sales)*100 as Profit_ratio
+sum(profit)/sum(sales)*100 as Profit_ratio
 from orders
+
+--Profit per Order
+SELECT 
+ORDER_ID
+,SUM(PROFIT) AS PROFIT
+FROM ORDERS
+GROUP BY 1
+;
+
+--Sales per Customer
+SELECT 
+CUSTOMER_ID
+,SUM(SALES) AS SALES
+FROM ORDERS
+GROUP BY 1
+;
 
 --Avg.Discount
 select
 avg(discount)*100
 from orders
 
+--Monthly Sales by Segment
+SELECT
+EXTRACT(MONTH FROM ORDER_DATE) AS MONTH
+,SEGMENT
+,SUM(SALES) AS SALES
+FROM ORDERS
+GROUP BY 1,2
+;
+
+--Monthly Sales by Product Category
+SELECT
+EXTRACT(MONTH FROM ORDER_DATE) AS MONTH
+,CATEGORY
+,SUM(SALES) AS SALES
+FROM ORDERS
+GROUP BY 1,2
+;
